3b2b6002b13c5917e0d39c4f14c69044cb1b1b68
Manashmodi <86552684+Manashmodi@users.noreply.github.com>
2023-03-23 11:24:08 +0530

    Add files via upload

---
+++ services/array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void printArray(){
+
+} 
+int main(){
+    int number[15];
+
+    cout<<"Value at 14 index"<<number[14]<<endl;
+
+    cout<<"value at 20 index"<<number[20]<<endl;
+    
+}
---
+++ services/extra.py
@@ -0,0 +1,10 @@
+import random 
+
+
+def sumup():
+    a=random.random()
+    b=random.random()
+    print (a+b)
+
+sumup()
+
